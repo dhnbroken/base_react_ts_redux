10d431ec0b0933e9c1567d2c16749d22bd2d3d5b
AMIT-PARMAR\amit_ <amit_p89@hotmail.com>
2023-05-06 12:16:21 +0530

    feature added: store configuration, persist key, store state type

---
+++ src/store/tools/constants.ts
@@ -0,0 +1,3 @@
+// redux constants
+export const KEY_ENCRYPT_STORE = process.env
+  .REACT_APP_KEY_ENCRYPT_STORE as string;
---
+++ src/store/tools/types.ts
@@ -0,0 +1,4 @@
+import { reducer } from "store/store";
+
+// create rootstate
+export type RootState = ReturnType<typeof reducer>;
