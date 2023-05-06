77ecd34a6986c9e22c703fd318445e703440b58e
AMIT-PARMAR\amit_ <amit_p89@hotmail.com>
2023-05-06 18:32:30 +0530

    added type void

--- src/store/modules/user-roles/actions/userRoles.thunk.ts
+++ src/store/modules/user-roles/actions/userRoles.thunk.ts
@@ -5,7 +5,7 @@ import { USER_ROLES_KEY } from "../../../tools/constants";
 
 export const getUserRoles = createAsyncThunk(
   `${USER_ROLES_KEY}/get`,
-  async (_, { rejectWithValue }) => {
+  async (_: void, { rejectWithValue }) => {
     const response = await apiUserRoles.apiGetUserRoles();
     if (!response.success) throw rejectWithValue(response);
     return response;
