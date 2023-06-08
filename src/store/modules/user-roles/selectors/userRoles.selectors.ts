3d561ff75adfae1871043c9ce7aecce3d670e2d9
AMIT-PARMAR\amit_ <amit_p89@hotmail.com>
2023-06-08 23:53:25 +0530

    created user roles selector moved story book to components folder

--- src/store/modules/user-roles/selectors/userRoles.selectors.ts
+++ src/store/modules/user-roles/selectors/userRoles.selectors.ts
@@ -1,12 +1,19 @@
 import { createSelector } from "@reduxjs/toolkit";
+import { get } from "lodash";
 
 import { USER_ROLES_KEY } from "../../../tools/constants";
 import { UserRolesState, RootState } from "../../../tools/types";
 
-export const getUserRoleState = (rootState: RootState): UserRolesState =>
-  rootState[USER_ROLES_KEY];
+const selectUserRolesState = (state: RootState): UserRolesState =>
+  state[USER_ROLES_KEY];
 
 export const selectGetRoleLoadingStatus = createSelector(
-  getUserRoleState,
-  (userRolesInfo) => userRolesInfo.getRoles.loadingStatus
+  selectUserRolesState,
+  (userRolesState) => userRolesState.getRoles.loadingStatus
+);
+
+export const selectUserRoles = createSelector(
+  selectUserRolesState,
+  (userRolesState) =>
+    get(userRolesState.getRoles.data, "response.userRoles", null)
 );
